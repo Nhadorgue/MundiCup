3881deef583430e28610d7191f42483964192fc3
Nhadorgue <gabrielmoura@gmail.com>
2020-10-14 17:19:50 -0300

    criando projeto

---
+++ MundiCupWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MundiCupWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
